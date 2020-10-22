28c32a3e8457b5c762b3993635c0d2d1e86ffea0
shernix <eduardo.santiago8@upr.edu>
2020-10-21 22:44:36 -0400

    Added Leaflet dependency and component.

---
+++ NestmaticsUI/src/app/map/map.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements AfterViewInit {
+  private map;
+  
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+tiles.addTo(this.map);
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 18.2208, -66.5901 ],
+      zoom: 9
+    });
+  }
+}
